03ecf3a45e51d94099533a1b25c414655d56435f
Kasper Olesen <kasperaolesen@gmail.com>
2017-10-31 22:52:02 +0100

    added factory functions and view6(commentview)

--- frontend/app/app/components/factories.js
+++ frontend/app/app/components/factories.js
@@ -4,36 +4,103 @@
 angular.module('myApp.factories', [])
         .factory('dataFactory', ['$http', '$window', function ($http, $window) {
 
-                var urlBase = 'api/link';
+                var urlBasePost = 'api/post';
+                var urlBaseComment = 'api/comment';
                 var dataFactory = {};
-                var flightInfo = [];
-                var ResObj = [];
-                var basePrice = 0;
+                var postInfo = [];
+                var commentInfo = [];
+                var commentCount = 0;
+//                dataFactory.addItem = function (item) {
+//                    basePrice = (item.totalPrice / item.numberOfSeats);
+//                    flightInfo.push(item);
+//                    $window.location.href = '#/view6';
+//                };
 
-                dataFactory.addItem = function (item) {
-                    basePrice = (item.totalPrice / item.numberOfSeats);
-                    flightInfo.push(item);
-                    $window.location.href = '#/view6';
+
+
+
+                //Dummy objects with test data
+
+                var post1 = {"username": "Tove",
+                    "post_type": "story",
+                    "pwd_hash": "ublamgata",
+                    "post_title": "This is a title for test post 1",
+                    "post_url": "post1LINK",
+                    "post_parent": -2,
+                    "hanesst_id": 325243,
+                    "post_text": "Posttext of post 1"};
+                var post2 = {"username": "Karsten",
+                    "post_type": "story",
+                    "pwd_hash": "u35ytgata",
+                    "post_title": "This is a title for test post 2",
+                    "post_url": "post222LINK",
+                    "post_parent": -3,
+                    "hanesst_id": 31678,
+                    "post_text": "Posttext of post 2"};
+                var post3 = {"username": "Bo",
+                    "post_type": "story",
+                    "pwd_hash": "ublsadgamgata",
+                    "post_title": "This is a title for test post 3",
+                    "post_url": "post3LINK",
+                    "post_parent": -1,
+                    "hanesst_id": 243,
+                    "post_text": "Posttext of post 3"};
+
+                var comment1 = {"post_id": "325243",
+                    "comment_id": "c352456",
+                    "comment_parent_id": "",
+                    "text": "This is a comment test",
+                    "author": "kirsten"
                 };
 
-                dataFactory.getAllFlightinfo = function (info) {
-                    return $http.get(urlBase + "/" + info);
+                var comment2 = {"post_id": "325243",
+                    "comment_id": "c2535",
+                    "comment_parent_id": "",
+                    "text": "This is also a comment test2325423",
+                    "author": "Henzaow"
                 };
 
-                dataFactory.addReservation = function (info) {
-                    return $http.post(urlBase + "/" + info, []);
+                var comment3 = {"post_id": "325243",
+                    "comment_id": "c28536",
+                    "comment_parent_id": "",
+                    "text": "This the last comment test",
+                    "author": "gunnar"};
+
+
+
+
+
+                dataFactory.getAllPosts = function () {
+//                    postInfo.push($http.get(urlBasePost));
+                    postInfo.push(post1, post2, post3);
+
+                    dataFactory.getAllComments("325243");
+                    dataFactory.commentCount();
+                    return postInfo;
                 };
 
-                dataFactory.getFlight = function () {
-                    return flightInfo;
+                dataFactory.getAllComments = function () {
+//                    commentInfo.push($http.get(urlBaseComment + "/" + info));
+                    commentInfo.push(comment1, comment2, comment3);
+                    return commentInfo;
                 };
 
-                dataFactory.getBasePrice = function () {
-                    return basePrice;
+                dataFactory.addPost = function (info) {
+                    return $http.post(urlBasePost + "/create/" + info);
                 };
 
-                dataFactory.createResObject = function (info) {
-                    ResObj.push(info);
+                dataFactory.addComment = function (info) {
+                    return $http.post(urlBaseComment + "/create/" + info);
+                };
+
+                dataFactory.commentCount = function () {
+                    console.log(postInfo);
+                    console.log(commentInfo);
+                    postInfo.forEach(function (postInfo) {
+                        postInfo.commentCount = 0;
+                        postInfo.commentCount = commentInfo.length;
+
+                    });
                 };
 
                 return dataFactory;
--- frontend/app/app/view1/view1.js
+++ frontend/app/app/view1/view1.js
@@ -2,24 +2,34 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'app/view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view1', {
+                    templateUrl: 'app/view1/view1.html',
+                    controller: 'View1Ctrl',
+                    controllerAs: 'ctrl'
+                });
+            }])
 
-  .controller('View1Ctrl', function ($http, $scope) {
+        .controller('View1Ctrl', ["$scope", "dataFactory", function ($scope, dataFactory) {
+                $scope.dataFactory = dataFactory;
 //          $http({
 //            method: 'GET',
-//            url: 'api/stories'
+//            url: 'api/post'
 //          }).then(function successCallback(res) {
-//            $scope.data = res.data.message;
+//            $scope.post = res.data.message;
 //          }, function errorCallback(res) {
 //            $scope.error = res.status + ": "+ res.data.statusText;
 //          });
-        });
+
+
+//        $scope.getCommentcount = function ()
+
+
+
+                $scope.posts = dataFactory.getAllPosts();
+
+
+            }]);
 
 
 //
@@ -29,3 +39,5 @@ angular.module('myApp.view1', ['ngRoute'])
 //        
 //        
 //}]);
+
+
